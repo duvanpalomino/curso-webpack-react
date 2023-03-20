6704e927d1726f760a23a2982d2d995412038c38
duvanpalomino <duvanpalomino@gmail.com>
2023-03-20 15:57:09 -0500

    deploy del proyecto con react.js y configurado con webpack a netlify

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,24 +1,16 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
-const { cleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/',
     },
-    mode: 'production',
+    mode: 'development',
     resolve: {
-        extensions: ['.js', '.jsx'],
-        alias: {
-            '@components': path.resolve(__dirname, 'src/components/'),
-            '@styles': path.resolve(__dirname, 'src/styles/'),
-        }
+        extensions: ['.js', '.jsx']
     },
     module: {
         rules: [
@@ -52,14 +44,14 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
             filename: '[name].css'
-        }),
-        new cleanWebpackPlugin(),
+        })
     ],
-    optimizacion: {
-        minimize: true,
-        minimizer: [
-            new CssMinimizerPlugin(),
-            new TerserWebpackPlugin(),
-        ]
-    }
+    devServer: {
+        static: {
+            directory:
+             path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 3006,
+    },
 }
